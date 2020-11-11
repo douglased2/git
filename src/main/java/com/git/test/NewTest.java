6aeefb6dce087b2117fd4fd5ffb1cf505a50aecc
gxs <1029363723@qq.com>
2020-11-11 10:46:54 +0800

    新新的

--- src/main/java/com/git/test/NewTest.java
+++ src/main/java/com/git/test/NewTest.java
@@ -2,6 +2,6 @@ package com.git.test;
 
 public class NewTest {
     public static void main(String[] args) {
-        System.out.println("新内蒙古大学！！@@");
+        System.out.println("新内蒙古大学！！@@333");
     }
 }
